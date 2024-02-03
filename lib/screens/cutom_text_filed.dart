e98fa74aaeeba8ae1ba4724282dd55f6c61410e5
HussienAboSoker <128934263+HussienAboSoker@users.noreply.github.com>
2024-02-03 17:31:10 +0200

    update search screen(put textfiled for search)

---
+++ lib/screens/cutom_text_filed.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({super.key});
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late TextEditingController _textEditingController ;
+  @override
+  //to initalize controller
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+  @override
+  //for cleat buffer data when go to anther screen 
+  void dispose() {
+  _textEditingController;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(onPressed: (){
+                  //note when you Clear()you dont must put set state 
+                  //this handled by flutter team 
+                  _textEditingController.clear();
+                }, icon:const Icon(Icons.delete)),
+                prefixIcon:const Icon(Icons.search),
+                hintText: 'Search...',
+             //   border: InputBorder.none,
+                enabledBorder: OutlineInputBorder(
+                  borderSide:const BorderSide(color: Colors.grey, width: 2.0),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:const BorderSide(color: Colors.blue, width: 2.0),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide:const BorderSide(color: Colors.red, width: 2.0),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                
+              ),
+            ),
+          ),
+        );
+  }
+}
--- lib/screens/searchscreen.dart
+++ lib/screens/searchscreen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:shop_smart_users/constans/imagepath.dart';
+import 'package:shop_smart_users/screens/cutom_text_filed.dart';
 
 import 'package:shop_smart_users/widget/empty_screen.dart';
 
 class SearchScreenPage extends StatelessWidget {
   const SearchScreenPage({super.key});
-
+  final isempty = true;
   @override
   Widget build(BuildContext context) {
-    return EmptySceen(
-      imagePath: ImagePath.emptySearch,
-      title: 'Empty search ',
-      subtitle: 'please seach ',
+    return Scaffold(
+      appBar:AppBar(
+        leading: Image.asset(ImagePath.shopingCart),
+        title:const Text("Search"),
+      ) ,
+      body:   isempty
+        ? EmptySceen(
+          imagePath: ImagePath.emptySearch,
+          title: 'Empty search ',
+          subtitle: 'please seach ',
+        )
+        :const CustomTextField() ,
     );
+    
+  
   }
 }

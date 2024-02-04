9d90998f4f77188e01ccdec41915821580bb3416
HussienAboSoker <128934263+HussienAboSoker@users.noreply.github.com>
2024-02-04 08:13:35 +0200

    update on custom product

--- lib/widget/custom_product.dart
+++ lib/widget/custom_product.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_smart_users/constans/imagepath.dart';
+import 'package:shop_smart_users/widget/customtext.dart';
+import 'package:shop_smart_users/widget/title.dart';
 
 class CustomProduct extends StatefulWidget {
   const CustomProduct({super.key});
@@ -10,16 +14,64 @@ class CustomProduct extends StatefulWidget {
 }
 
 class _CustomProductState extends State<CustomProduct> {
-  
   @override
   Widget build(BuildContext context) {
-    Size size=MediaQuery.of(context).size;
-    return Column(
-      children: [
-        FancyShimmerImage(imageUrl:"assets/images/sahs.jpeg",
-        width:double.infinity ,
-        height:size.height*0.2 ,),
-      ],
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: () {
+        log("go to scrren product");
+      },
+      child: Column(
+        children: [
+          FancyShimmerImage(
+            imageUrl: "assets/images/sahs.jpeg",
+            width: double.infinity,
+            height: size.height * 0.2,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Flexible(
+                  flex: 5,
+                  child: CustomTitle(
+                      label:
+                          'fsfkdfskfdklfskd dfdsfdsf dfs fsdfs fsd sfd fsfs  fsdfsdf dfs sfdfs f fs fsfdsfs sf')),
+              Flexible(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.favorite),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Flexible(
+                  flex: 1,
+                  child: CustomsubTitle(
+                    label: '123\$',
+                    color: Colors.green,
+                  )),
+              Flexible(
+                child: Material(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.blue,
+                  child: InkWell(
+                    onTap: () {},
+                    splashColor: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                    child: const Padding(
+                      padding: EdgeInsets.all(4.0),
+                      child: Icon(Icons.add_shopping_cart_outlined),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
